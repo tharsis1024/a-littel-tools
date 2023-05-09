327cfe304acf6a567a1f94ebf6a2013e14c3b700
tharsis1024 <102180824+tharsis1024@users.noreply.github.com>
2023-05-09 10:27:32 +0800

    Add files via upload

--- wifi.py
+++ wifi.py
@@ -8,16 +8,13 @@ def main():
     parser.add_argument("-mode", type=str, default="s")
     parser.add_argument("-w", type=str, default="/usr/share/wordlists/fasttrack.txt")
     parser.add_argument("-bssid", type=str, default="0")
-#    parser.add_argument("-pack", type=str, default="pack")
-
-
     args = parser.parse_args()
+    
     device = args.device
     dssid = args.dssid
     mode = args.mode
     bssid = args.bssid
     wordlist = args.w
-#    pack = args.pack
     pack="pack-01.ivs"
 
     if "mon" in device:
@@ -27,13 +24,13 @@ def main():
         device = device+"mon"
     
     if mode == "s" and bssid == "0":
-        wifi_scan1(device,pack)
+        wifi_scan1(device)
     
     elif mode == "s" and bssid != "0":
-        wifi_scan2(device,pack,bssid)
+        wifi_scan2(device,bssid)
 
     elif mode == "c" and bssid !="0":
-        wifi_crack(bssid,wordlist,pack)
+        wifi_crack(wordlist,pack)
     
     elif mode == "a" :
         wifi_attack(device,bssid,dssid)
@@ -42,20 +39,15 @@ def main():
         os.system(exit)
  
 def wifi_scan1(device):
-#    pack1 = "-w " + pack
-#    os.system("airodump-ng " + pack1 +" "+device)
-
     os.system("airodump-ng -w pack "+device)
 
 def wifi_scan2(device,bssid):  
-#    pack2 ="-w " + pack
-#    os.system("airodump-ng "+ pack2 + " "+device+" --bssid "+bssid)
     os.system("airodump-ng -w pack "+device+" --bssid "+bssid)
 
 def wifi_attack(bssid,dssid,device):
     os.system("aireplay-ng -0 2 -a" + bssid + " -c "+dssid + " " +device)
 
-def wifi_crack(bssid,wordlist,pack):
+def wifi_crack(wordlist,pack):
     os.system("aircrack-ng "+ pack +" -w "+ wordlist)
 
 if __name__ == "__main__":
